cd3fc4f84f5ab731c60b226c098e59ee3e786fc4
TrevorThoele <t_thoele@yahoo.com>
2019-09-11 19:04:37 -0500

    Before experimentation.

--- Inscription/InvalidArchiveDirection.cpp
+++
@@ -1,8 +0,0 @@
-#include "InvalidArchiveDirection.h"
-
-namespace Inscription
-{
-    InvalidArchiveDirection::InvalidArchiveDirection() :
-        Exception("Attempted operation in the opposite direction of the archive; saving for an input archive or loading for an output archive.")
-    {}
-}
