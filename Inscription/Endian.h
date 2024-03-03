ef289572ed5f70f8fd22dfbfec73b9bebf3cdbda
Trevor Thoele <t_thoele@yahoo.com>
2024-03-02 23:48:53 -0600

    Changes.

--- Inscription/Endian.h
+++ Inscription/Endian.h
@@ -11,15 +11,15 @@ namespace Inscription
     void SwapBytes(T& t)
     {
         size_t size = sizeof(t);
-        if (size < 2)
-            return;
+        if (size >= 2)
+        {
+            char* byteArray = reinterpret_cast<char*>(&t);
 
-        char* byteArray = reinterpret_cast<char*>(&t);
-
-        const auto halfSize = size / 2;
-        const auto sizeCompare = --size;
-        for (size_t loop = 0; loop < halfSize; ++loop)
-            std::swap(byteArray[loop], byteArray[sizeCompare - loop]);
+            const auto halfSize = size / 2;
+            const auto sizeCompare = --size;
+            for (size_t loop = 0; loop < halfSize; ++loop)
+                std::swap(byteArray[loop], byteArray[sizeCompare - loop]);
+        }
     }
 
     template<class T>
